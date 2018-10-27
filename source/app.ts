28d454e7754e8a1254dcace3a6929ab32d746e3e
ElroyDolleman <elroydolleman@gmail.com>
2018-10-27 12:57:35 +0200

    Added html page for the game

--- source/app.ts
+++ source/app.ts
@@ -3,7 +3,39 @@ const SCREEN_HEIGHT: number = 600;
 
 class GameScene extends Phaser.Scene
 {
+    constructor()
+    {
+        super({ key: 'GameScene', active: true});
+    }
+
+    preload()
+    {
+        
+    }
+
+    create()
+    {
+
+    }
+
+    update()
+    {
+
+    }
+
+    resize() 
+    {
+        var canvas = game.canvas, width = window.innerWidth, height = window.innerHeight;
+        var wratio = width / height, ratio = canvas.width / canvas.height;
     
+        if (wratio < ratio) {
+            canvas.style.width = width + "px";
+            canvas.style.height = (width / ratio) + "px";
+        } else {
+            canvas.style.width = (height * ratio) + "px";
+            canvas.style.height = height + "px";
+        }
+    }
 }
 
 var config = {
